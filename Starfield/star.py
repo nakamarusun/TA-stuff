71cc81ea9b294952849e761189785ddfe7ead5bc
Jason Christian H <nakamarujc@gmail.com>
2020-09-23 11:30:36 +0700

    Improved timer functionality to know how many seconds have passed since the trigger, and how many times to repeat.

--- Starfield/star.py
+++ Starfield/star.py
@@ -12,29 +12,43 @@ class Star_Manager:
     max_speed = 0.035
     min_speed = 0.005
 
+    # Variables to store the min and max time in milliseconds to spawn a star.
+    every_min = 3
+    every_max = 6
+
     def __init__(self):
         super().__init__()
 
         # Star list
         self.star_list = [] # This is list inside of list, [[x, y, speed, size], ...]
-        self.star_timer = 0
+        self.rand_milli = random.randint(Star_Manager.every_min, Star_Manager.every_max) # Amount of time to wait
+        self.star_timer = util.get_milli() + self.rand_milli # Timer to spawn stars
 
     def step(self):
 
         mid = util.get_middle_res()
 
-        # Simple timer functionality
-        if (self.star_timer < util.get_milli()):
-            # Coords
-            coords = [random.randint(-util.resolution[0]//2, util.resolution[0]//2), random.randint(-util.resolution[1]//2, util.resolution[1]//2)]
-            # Append a new star in the array
-            self.star_list.append([
-                *coords,
-                1 + Star_Manager.min_speed + (random.random() * Star_Manager.max_speed),
-                abs(coords[0]/mid[0]) + abs(coords[1]/mid[1]) * 5
-            ])
+        cur_time = util.get_milli()
+
+        # Creates new star objects based on the timer set.
+        if (self.star_timer < cur_time):
+            repetition = int(round((cur_time - self.star_timer) / self.rand_milli))
+
+            # Creates stars based on timer
+            for i in range(repetition):
+                # Coords
+                coords = [random.randint(-util.resolution[0]//2, util.resolution[0]//2), random.randint(-util.resolution[1]//2, util.resolution[1]//2)]
+                # Append a new star in the array
+                self.star_list.append([
+                    *coords,
+                    1 + Star_Manager.min_speed + (random.random() * Star_Manager.max_speed),
+                    abs(coords[0]/mid[0]) + abs(coords[1]/mid[1]) * 5
+                ])
+
             # Restart timer
-            self.star_timer = util.get_milli() + random.randint(1, 2)
+            self.rand_milli = random.randint(Star_Manager.every_min, Star_Manager.every_max)
+            self.star_timer = util.get_milli() + self.rand_milli
+
 
         counter = 0 # Counter to count how many items has been iterated.
         pop_list = [] # List of objects that need to be deleted from the list.
